4fefcd8e9a994672bd3595c9aac6cc8cf4be44cc
Ajitsy <sanghera.p@gmail.com>
2015-08-12 16:02:41 +0100

    updated data mapper for Heroku

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'data_mapper'
 env = ENV['RACK_ENV'] || 'development'
 
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link' # require each model individually - the path may vary depending on your file structure.
 
